22434ebf818989d7dc10a9862c182e4d2d55d29e
GagenvirGill <gagenvirg@gmail.com>
2025-04-27 19:18:55 -0700

    Implemented the chat and message models

---
+++ townhall/chats/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+from users.models import User
+
+
+class Chat(models.Model):
+    participants = models.ManyToManyField(User, related_name="chats")
+    name = models.CharField(max_length=127)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Message(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    chat_id = models.ForeignKey(Chat, on_delete=models.DO_NOTHING)
+    content = models.TextField()
+    image_content = models.ImageField(upload_to="post_images", null=True, blank=True)
+    sent_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return str(self.id)
--- townhall/townhall/settings.py
+++ townhall/townhall/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "debug_toolbar",
     "users",
     "posts",
+    "chats",
 ]
 
 MIDDLEWARE = [
