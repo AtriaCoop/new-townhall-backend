fb3ad8e25793bceb4768302e4b1d7843f56a14a4
gwb2 <genthebestperson@gmail.com>
2025-05-09 15:23:54 -0700

    Write the dao and service methods. I have not yet written the test cases. I will do that soon

--- townhall/chats/types.py
+++ townhall/chats/types.py
@@ -1,8 +1,16 @@
-from typing import List
+from typing import List, Optional
 from dataclasses import dataclass
-
+from datetime import datetime
 
 @dataclass
 class CreateChatData:
     participant_ids: List[int]
     name: str
+
+@dataclass
+class CreateMessageData:
+    sender_id: int
+    chat_id: int
+    content: str
+    image_content: Optional[str]
+    sent_at: datetime
