8573a1b1b3cc2ec53b33494fea9bc25177c7b975
Ryan <ryeean16@gmail.com>
2025-05-22 16:45:30 -0700

    cloudinary fix

--- townhall/townhall/settings.py
+++ townhall/townhall/settings.py
@@ -198,8 +198,15 @@ DEBUG_TOOLBAR_PANELS = [
     "debug_toolbar.panels.profiling.ProfilingPanel",
 ]
 
+
 # CLOUDINARY
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
 
 cloudinary.config( 
   cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'), 
