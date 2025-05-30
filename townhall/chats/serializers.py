561fe5f33a33d7f37e239ac93fdf58a88d317333
gwb2 <genthebestperson@gmail.com>
2025-05-30 14:08:52 -0700

    Create endpoint to create a message, and write tests

--- townhall/chats/serializers.py
+++ townhall/chats/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-from chats.models import Chat
+from chats.models import Chat, Message
 from users.models import User
+from users.serializers import UserMiniSerializer
 
 
 class ChatSerializer(serializers.ModelSerializer):
@@ -12,3 +13,39 @@ class ChatSerializer(serializers.ModelSerializer):
         model = Chat
         fields = ["id", "name", "participants", "created_at"]
         read_only_fields = ["id", "created_at"]
+
+
+class ChatMiniSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chat
+        fields = ["id", "name"]
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    user = UserMiniSerializer(read_only=True)
+    user_id = serializers.PrimaryKeyRelatedField(
+        many=False, queryset=User.objects.all(), source="user"
+    )
+
+    chat = ChatMiniSerializer(read_only=True)
+    chat_id = serializers.PrimaryKeyRelatedField(
+        many=False, queryset=Chat.objects.all(), source="chat"
+    )
+
+    content = serializers.CharField(required=True, allow_blank=False)
+    image_content = serializers.ImageField(required=False, allow_null=True)
+    image_content = serializers.ImageField(required=False, allow_null=True)
+
+    class Meta:
+        model = Message
+        fields = [
+            "id",
+            "user",
+            "user_id",
+            "chat",
+            "chat_id",
+            "content",
+            "image_content",
+            "sent_at",
+        ]
+        read_only_fields = ["id"]
