3215d0f16364a3744b21599139e89dd236cbdcc3
Ryan <ryeean16@gmail.com>
2025-05-30 15:20:47 -0700

    ensure_csrf_cookie fix

--- townhall/users/views.py
+++ townhall/users/views.py
@@ -24,7 +24,7 @@ from .services import UserServices
 @ensure_csrf_cookie
 def get_csrf_token(request):
     token = get_token(request)
-    return JsonResponse({"detail": "CSRF cookie set"})
+    return JsonResponse({"csrfToken": token})
 
 
 # USER LOGIN
