4c62dba532e18992e4ac079732d77993a1810189
Ryan <ryeean16@gmail.com>
2025-06-09 16:18:48 -0700

    consumer and routing update for group chats (websocketes)

--- townhall/chats/consumers.py
+++ townhall/chats/consumers.py
@@ -89,3 +89,41 @@ class UserConsumer(AsyncWebsocketConsumer):
 
     async def user_message(self, event):
         await self.send(text_data=json.dumps(event))
+
+
+class GroupConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.group_name = self.scope['url_route']['kwargs']['group_name']
+        self.room_group_name = f"group_{self.group_name}"
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data['message']
+        sender = data['sender']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'group_message',
+                'message': message,
+                'sender': sender,
+            }
+        )
+
+    async def group_message(self, event):
+        await self.send(text_data=json.dumps({
+            'message': event['message'],
+            'sender': event['sender'],
+        }))
--- townhall/townhall/routing.py
+++ townhall/townhall/routing.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
-from chats.consumers import ChatConsumer, UserConsumer
+from chats.consumers import ChatConsumer, UserConsumer, GroupConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/chats/(?P<chat_id>\w+)/$', ChatConsumer.as_asgi()),
+    re_path(r'ws/groups/(?P<group_name>[^/]+)/$', GroupConsumer.as_asgi()),
     re_path(r'ws/users/(?P<user_id>\w+)/$', UserConsumer.as_asgi()),
 ]
