4cdca0f41b93cf42dd892c69aee429ba2e196a93
Andrew <andrew-jung@hotmail.com>
2025-09-27 18:39:49 -0700

    Create ReportedPosts table and make DB migration

--- townhall/posts/models.py
+++ townhall/posts/models.py
@@ -26,3 +26,12 @@ class Comment(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+class ReportedPosts(models.Model):
+    id = models.AutoField(primary_key=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return str(self.id)
