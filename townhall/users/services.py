d46269f92569757d57b8d39fa7552e06a9c541ab
Ethan Miranda <emirand2@uwo.ca>
2025-10-18 13:48:16 -0400

    afr-158: fixed linting issues

--- townhall/users/services.py
+++ townhall/users/services.py
@@ -154,7 +154,7 @@ class UserServices:
     def get_all_tags() -> typing.List[str]:
         tags = UserDao.get_all_tags()
         return [tag.name for tag in tags]
-      
+
     def get_tags_for_user(user_id: int) -> typing.List[str]:
         try:
             tags = UserDao.get_tags_for_user(user_id=user_id)
