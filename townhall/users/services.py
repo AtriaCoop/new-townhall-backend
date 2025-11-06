1c38f8ba6cf182702742def9e9c7d15962cc63b0
Ethan Miranda <emirand2@uwo.ca>
2025-11-06 17:05:31 -0500

    afr-161: service to get users by tags

--- townhall/users/services.py
+++ townhall/users/services.py
@@ -165,3 +165,6 @@ class UserServices:
             return tags
         except User.DoesNotExist:
             raise ValidationError(f"User with the given id: {user_id}, does not exist.")
+
+    def get_users_by_tags(tag_names: typing.List[str]) -> QuerySet[User]:
+        return UserDao.get_users_by_tags(tag_names=tag_names)
